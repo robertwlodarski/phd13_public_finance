e0f31ef8e83f04f6576c3af6b2c33fed598f37fb
Rob Włodarski <81385057+robertwlodarski@users.noreply.github.com>
2026-03-24 21:34:55 +0000

    Final main file finished

--- Main.jl
+++ Main.jl
@@ -9,6 +9,7 @@
 ## 1. Packages & load functions 
 using Parameters, QuantEcon, LinearAlgebra, Roots, Printf, Plots, Distributions, StatsBase, Random, Dierckx 
 using Statistics, Plots
+using DataFrames, Distributions, Random, GLM
 include("scripts/ModelInfrastructure.jl")
 include("scripts/Functions.jl")
 
@@ -18,9 +19,13 @@ include("scripts/Functions.jl")
 @time fnNonStochasticSimulation!(UsedParameters,EndoMain;end_labour=true)
 @time fnMonteCarlo!(UsedParameters, EndoMain; end_labour = true)
 
-## 3. Comparing two methods of calculating key moments 
+## 3. Compare different simulation methods 
 include("analysis/ComparingSimulations.jl")
 
-## 4. Simulation and estimation
+
+## 4. Estimation 
+include("analysis/SyntheticSampleAnalysis.jl")
+include("analysis/EstimationGamma.jl")
+include("analysis/EstimationChi.jl")
 
 ## 5. Real vs. model data statistics
